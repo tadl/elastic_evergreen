f341eddbe39c40cb6d61af86af47b44ab66fca5a
Scott Morey <swmorey@gmail.com>
2015-09-03 11:58:59 -0400

    added full text search with prefix matching to make getting results easier

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,7 +3,7 @@ class Record < ActiveRecord::Base
 	include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
   	
-  	pg_search_scope :search_title, :against => [:title]
-  	pg_search_scope :search_author, :against => [:author]
-  	pg_search_scope :search_keyword, :against => [:author, :title]
+  	pg_search_scope :search_title, :against => [:title], :using => {:tsearch => {:prefix => true}}
+  	pg_search_scope :search_author, :against => [:author], :using => {:tsearch => {:prefix => true}}
+  	pg_search_scope :search_keyword, :against => [:author, :title], :using => {:tsearch => {:prefix => true}}
 end
