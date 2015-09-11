920c2d9c070a34c39ca926b1c072de3661b4173a
Jeff Godin <jgodin@tadl.org>
2015-09-11 11:16:08 -0400

    Add field: title.folded
    This gives us a new title.folded field which will be filtered as
    lowercase + asciifolded. This means that we can search (probably
    with a most_fields multi_match query) for "pokemon" and find
    "Pokémon" titles.
    
    Index will be re-created with the following from console:
    
    Record.__elasticsearch__.create_index! force: true
    Record.import
    
    Signed-off-by: Jeff Godin <jgodin@tadl.org>

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,4 +6,10 @@ class Record < ActiveRecord::Base
   	pg_search_scope :search_title, :against => [:title], :using => {:tsearch => {:prefix => true}}
   	pg_search_scope :search_author, :against => [:author], :using => {:tsearch => {:prefix => true}}
   	pg_search_scope :search_keyword, :against => [:author, :title], :using => {:tsearch => {:prefix => true}}
+
+    settings analysis: {analyzer: {folding: {tokenizer: 'standard', filter: ['lowercase', 'asciifolding']}}} do
+        mappings do
+            indexes :title, analyzer: 'english', fields: {folded: {type: 'string', analyzer: 'folding'}}
+        end
+    end
 end
