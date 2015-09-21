56ad87328ce3e6141bccbc85270d56f9e426854d
Scott Morey <swmorey@gmail.com>
2015-09-21 12:28:35 -0400

    getting ready to show

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,23 +3,40 @@ class MainController < ApplicationController
 	respond_to :html, :json
   def index
   	@record_count = Record.all.count
-  	keyword = URI.unescape(params[:keyword]) rescue ''
-  	title = URI.unescape(params[:title]) rescue ''
-  	author = URI.unescape(params[:author]) rescue ''
+  	search_term = URI.unescape(params[:query]) rescue ''
   	search_type = params[:search_type] rescue nil
     if search_type.nil? || search_type == 'keyword'
-        @records = Record.search(keyword)
-    elsif search_type == 'pgkeyword'
-        # Use pg_search here
-    	@records = Record.search_keyword(keyword + ' ' + title + ' ' + author)
+    	response = Record.search query: 
+      {
+        bool:
+        { 
+          should:[ 
+            {
+              multi_match: {
+                query: search_term,
+                fields: ['title^3','author^2', 'abstract'],
+              }
+            },
+            {
+              multi_match: {
+                query: search_term,
+                fields: ['title^3','author^4', 'abstract'],
+                fuzziness: 2
+              }
+            }
+          ]
+        }
+      },
+      size: 24,
+      from: 0
   	elsif search_type == 'author'
         # response = Record.search query: {match: { author: {query: author, fuzziness: 1} } }
-        response = Record.search min_score: 0.1, query: {bool:{ should:[{match: {author: author}}, {match_phrase: {author: author}}, {fuzzy: {author: author}}]}}
-        # @records = response
-        @records = response.records
+        response = Record.search min_score: 0.1, query: {bool:{ should:[{match: {author: search_term}}, {match_phrase: {author: search_term}}, {fuzzy: {author: search_term}}]}}
+        
   	elsif search_type == 'title'
-        @records = Record.search query: {multi_match: {type: 'most_fields', query: title, fields: ['title', 'title.folded']} }
+        response = Record.search query: {multi_match: {type: 'most_fields', query: search_term, fields: ['title', 'title.folded']} }
   	end
+    @records = response.records
   	respond_to do |format|
       format.html
       format.json {render json: @records}
