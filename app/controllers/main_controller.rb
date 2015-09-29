f201069613bbe2c01fb7c65b907ded6e6485ed21
Scott Morey <swmorey@gmail.com>
2015-09-29 11:22:51 -0400

    moved search into model and playing around with optimization

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,45 +2,21 @@ class MainController < ApplicationController
 	require 'open-uri'
 	respond_to :html, :json
   def index
-  	@record_count = Record.all.count
   	search_term = URI.unescape(params[:query]) rescue ''
   	search_type = params[:search_type] rescue nil
+    search_job = Searchjob.new
     if search_type.nil? || search_type == 'keyword'
-    	response = Record.search query: 
-      {
-        bool:
-        { 
-          should:[ 
-            {
-              multi_match: {
-                query: search_term,
-                fields: ['title^3','author^2', 'abstract'],
-              }
-            },
-            {
-              multi_match: {
-                query: search_term,
-                fields: ['title^3','author^2', 'abstract'],
-                fuzziness: 2
-              }
-            }
-          ]
-        }
-      },
-      size: 24,
-      from: 0,
-      min_score: 0.4
+    	response = search_job.keyword(search_term)
   	elsif search_type == 'author'
-        # response = Record.search query: {match: { author: {query: author, fuzziness: 1} } }
-        response = Record.search min_score: 0.1, query: {bool:{ should:[{match: {author: search_term}}, {match_phrase: {author: search_term}}, {fuzzy: {author: search_term}}]}}
-        
+      response = search_job.author(search_term)
   	elsif search_type == 'title'
-        response = Record.search query: {multi_match: {type: 'most_fields', query: search_term, fields: ['title', 'title.folded']} }
+      response = search_job.title(search_term)
+    elsif search_type == 'record_id'
+      response = search_job.record_id(search_term)
   	end
-    @records = response.records
   	respond_to do |format|
       format.html
-      format.json {render json: @records}
+      format.json {render json: response}
     end
   end
 
---
+++ app/models/searchjob.rb
@@ -0,0 +1,116 @@
+class Searchjob
+  def keyword(search_term)
+    results = Record.search query: 
+      {
+        bool:{ 
+          should:[ 
+            {
+              multi_match: {
+                type: 'most_fields', 
+                query: search_term, 
+                fields: ['title', 'title.folded','author'],
+                boost: 3
+              }
+            },
+            {
+              multi_match: {
+                type: 'best_fields', 
+                query: search_term, 
+                fields: ['title', 'title.folded', 'author'],
+                boost: 2,
+                fuzziness: 1
+              }
+            }
+            # {
+            #   multi_match: {
+            #     type: 'best_fields', 
+            #     query: search_term, 
+            #     fields: ['author'],
+            #     boost: 2,
+            #   }
+            # }
+            # {
+            #   multi_match: {
+            #     type: 'phrase', 
+            #     query: search_term, 
+            #     fields: ['title', 'title.folded'],
+            #     fuzziness: 1,
+            #   }
+            # },
+   
+            # {
+            #   match_phrase: {
+            #     author: search_term
+            #   }
+            # }, 
+            # {
+            #   fuzzy: {
+            #     author: search_term
+            #   }
+            # },
+            # {
+            #   multi_match: {
+            #     query: search_term,
+            #     fields: ['abstract','author'],
+            #     fuzziness: 2
+            #   }
+            # }
+          ]
+        }
+      },
+      size: 50,
+      from: 0,
+      min_score: 0.3
+    return massage_response(results)
+  end
+
+  def author(search_term)
+    results = Record.search query: 
+    {
+      bool:{ 
+        should:[
+          {
+            match: 
+              {author: search_term}}, 
+              {match_phrase: {author: search_term}}, 
+              {fuzzy: {author: search_term},
+          }
+        ]
+      }
+    },
+    size: 24,
+    min_score: 0.3
+    return massage_response(results)
+  end
+
+  def title(search_term)
+    results = Record.search query: 
+      {
+        multi_match: {
+          type: 'most_fields', 
+          query: search_term, 
+          fields: ['title', 'title.folded']
+        } 
+      }
+    return massage_response(results)
+  end
+
+  def record_id(search_term)
+    results = Record.search query:
+    {
+      term:{
+        "id": search_term
+      }
+    }
+    return massage_response(results)
+  end
+
+  def massage_response(results)
+    massaged_response = results.map do |e|
+      record = e['_source']
+      record['score'] = e['_score']
+      record
+    end
+    return massaged_response
+  end
+end
